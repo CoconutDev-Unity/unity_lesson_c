dd99fdc545e47de090a6a83d186d34e1bec2bb62
Artem Derevianko <artems.derevianko@gmail.com>
2023-04-01 01:48:28 +0200

    updated gitignore file and added to commit all necessary project files

---
+++ Assets/Scripts/FPS/CrateOpener.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateOpener : MonoBehaviour
+{
+    [SerializeField] private bool _isOpen;
+    [SerializeField] private float _openCloseTime = 1f;
+    private Vector3 _startPosition;
+    private Vector3 _targetPosition;
+    [SerializeField] private float _moveForwardBy = 0.3f;
+    private float openToCloseLerp;
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+        _targetPosition = transform.position - transform.forward * _moveForwardBy;
+    }
+
+    IEnumerator Open()
+    {
+        while (openToCloseLerp < 1)
+        {
+            openToCloseLerp += Time.deltaTime / _openCloseTime;
+            transform.position = Vector3.Lerp(_startPosition, _targetPosition, openToCloseLerp);
+            yield return null;
+        }
+    }
+
+    IEnumerator Close()
+    {
+        while (openToCloseLerp > 0)
+        {
+            openToCloseLerp -= Time.deltaTime / _openCloseTime;
+            transform.position = Vector3.Lerp(_startPosition, _targetPosition, openToCloseLerp);
+            yield return null;
+        }
+    }
+
+    public void openOrClose()
+    {
+        if (_isOpen)
+        {
+            StartCoroutine(Open());
+        } else
+        {
+            StartCoroutine(Close());
+        }
+    }
+}
---
+++ Assets/Scripts/FPS/DraggableObject.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class DraggableObject : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private Vector3 _intialScape;
+    [SerializeField] private Vector3 _targetPosition;
+    [SerializeField] private bool _follow;
+    [SerializeField] private float _followSpeed = 15f;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (!_follow)
+            return;
+
+        Vector3 moveDirection = _targetPosition - _rigidbody.position;
+        _rigidbody.velocity = moveDirection * _followSpeed;
+    }
+
+    public void StartFollowingObject()
+    {
+        _follow = true;
+    }
+
+    public void SetTargetPosition(Vector3 newTargetPosition)
+    {
+        _targetPosition = newTargetPosition;
+    }
+
+    public void StopFollowingObject()
+    {
+        _follow = false;
+        _rigidbody.velocity = Vector3.zero;
+    }
+}
